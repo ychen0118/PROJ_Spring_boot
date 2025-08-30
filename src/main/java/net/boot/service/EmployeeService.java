e74d99049e4d3195142df3b621c660e522039e4d
ychen105 <chen_yigang@hotmail.com>
2025-08-30 12:37:12 -0500

    init check in

---
+++ src/main/java/net/boot/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package net.boot.service;
+
+import java.util.List;
+import net.boot.model.Employee;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+}
---
+++ src/main/java/net/boot/service/EmployeeServiceImpl.java
@@ -0,0 +1,43 @@
+package net.boot.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.boot.model.Employee;
+import net.boot.repository.EmployeeRepository;
+
+@Service
+public class EmployeeServiceImpl implements  EmployeeService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        this.employeeRepository.save(employee);
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> optional = employeeRepository.findById(id);
+        Employee employee = null;
+        if (optional.isPresent()) {
+            employee = optional.get();
+        } else {
+            throw new RuntimeException("Employee not found for id :: " + id);
+        }
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployeeById(long id) {
+        this.employeeRepository.deleteById(id);
+    }
+}
